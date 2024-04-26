96b76427f8981bc4db375325df7dd711459f614e
Vedant <vedant.iosfwt@gmail.com>
2024-04-26 23:11:51 +0530

    Font Setup

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  assets: [
+    './source/assets/fonts/AbhayaLibre',
+    './source/assets/fonts/Cairo',
+    './source/assets/fonts/ABeeZee',
+  ],
+};
---
+++ source/themes/fonts.ts
@@ -0,0 +1,20 @@
+export const AbhayaLibre = {
+  bold: 'AbhayaLibre-Bold',
+  extraBold: 'AbhayaLibre-ExtraBold',
+  medium: 'AbhayaLibre-Medium',
+  regular: 'AbhayaLibre-Regular',
+  semiBold: 'AbhayaLibre-SemiBold',
+};
+
+export const Ciaro = {
+  black: 'Cairo-Black',
+  bold: 'Cairo-Bold',
+  extraBold: 'Cairo-ExtraBold',
+  extraLight: 'Cairo-ExtraLight',
+  light: 'Cairo-Light',
+  medium: 'Cairo-Medium',
+  regular: 'Cairo-Regular',
+  semiBold: 'Cairo-SemiBold',
+};
+
+export const ABeeZee = {italic: 'ABeeZee-Italic', regular: 'ABeeZee-Regular'};
